ff0e009b5dc93d514dec3dc154a9225b6d466181
Arslan Ibragimov <a.z.ibragimov@tbank.ru>
2026-02-24 19:15:10 +0500

    check winner(#5)

--- index.js
+++ index.js
@@ -3,16 +3,96 @@ const ZERO = 'O';
 const EMPTY = ' ';
 let field = new Array();
 let fieldDimension = 3;
+let isGameOver = false;
 const container = document.getElementById('fieldWrapper');
 
 startGame();
 addResetListener();
 
-function startGame () {
+function startGame() {
     renderGrid(fieldDimension);
 }
 
-function renderGrid (dimension) {
+function idx(r, c) {
+    return r * fieldDimension + c;
+}
+
+function checkWinner() {
+    for (let r = 0; r < fieldDimension; r++) {
+        let res = checkWinnerRow(r, CROSS);
+        if (res) return res;
+
+        res = checkWinnerRow(r, ZERO);
+        if (res) return res;
+    }
+
+    for (let c = 0; c < fieldDimension; c++) {
+        let res = checkWinnerCol(c, CROSS);
+        if (res) return res;
+
+        res = checkWinnerCol(c, ZERO);
+        if (res) return res;
+    }
+
+    let res = checkWinnerDiagonal(CROSS);
+    if (res) return res;
+
+    res = checkWinnerDiagonal(ZERO);
+    if (res) return res;
+
+    return undefined;
+}
+
+function checkWinnerRow(row, symbol) {
+    const cells = [];
+
+    for (let col = 0; col < fieldDimension; col++) {
+        const v = field[idx(row, col)];
+        if (v !== symbol)
+            return undefined;
+        cells.push([row, col, symbol]);
+    }
+
+    return { winner: symbol, cells };
+}
+
+function checkWinnerCol(col, symbol) {
+    const cells = [];
+
+    for (let row = 0; row < fieldDimension; row++) {
+        const v = field[idx(row, col)];
+        if (v !== symbol) return undefined;
+        cells.push([row, col, symbol]);
+    }
+
+    return { winner: symbol, cells };
+}
+
+function checkWinnerDiagonal(symbol) {
+    let cells = [];
+    for (let i = 0; i < fieldDimension; i++) {
+        const v = field[idx(i, i)];
+        if (v !== symbol) {
+            cells = null;
+            break;
+        }
+        cells.push([i, i, symbol]);
+    }
+    if (cells) return { winner: symbol, cells };
+
+    cells = [];
+    for (let i = 0; i < fieldDimension; i++) {
+        const r = i;
+        const c = fieldDimension - 1 - i;
+        const v = field[idx(r, c)];
+        if (v !== symbol) return undefined;
+        cells.push([r, c, symbol]);
+    }
+
+    return { winner: symbol, cells };
+}
+
+function renderGrid(dimension) {
     container.innerHTML = '';
     field = new Array(dimension * dimension).fill(EMPTY);
 
@@ -28,40 +108,59 @@ function renderGrid (dimension) {
     }
 }
 
-function cellClickHandler (row, col) {
+function cellClickHandler(row, col) {
+    if (isGameOver)
+        return;
+
     if (field[row * fieldDimension + col] === EMPTY) {
         console.log(`Clicked on cell: ${row}, ${col}`);
         renderSymbolInCell(CROSS, row, col);
         field[row * fieldDimension + col] = CROSS;
+
+        const timeout = setTimeout(() => {
+            const winner = checkWinner();
+            if (winner) {
+                isGameOver = true;
+                alert(`Winner: ${winner.winner}`);
+                drawWinCells(winner.cells);
+                return;
+            }
+            }, 100
+        );
     }
 }
 
-function renderSymbolInCell (symbol, row, col, color = '#333') {
+function renderSymbolInCell(symbol, row, col, color = '#333') {
     const targetCell = findCell(row, col);
 
     targetCell.textContent = symbol;
     targetCell.style.color = color;
 }
 
-function findCell (row, col) {
+function findCell(row, col) {
     const targetRow = container.querySelectorAll('tr')[row];
     return targetRow.querySelectorAll('td')[col];
 }
 
-function addResetListener () {
+function addResetListener() {
     const resetButton = document.getElementById('reset');
     resetButton.addEventListener('click', resetClickHandler);
 }
 
-function resetClickHandler () {
+function resetClickHandler() {
     const gridRows = container.querySelectorAll('tr');
     for (const row of gridRows) {
-        for (const cell of row.querySelectorAll('td'))
-        {
+        for (const cell of row.querySelectorAll('td')) {
             cell.textContent = EMPTY;
             cell.style.color = '#333';
         }
     }
+
+    for (let i = 0; i < field.length; i++) {
+        field[i] = EMPTY;
+    }
+
+    isGameOver = false;
 }
 
 function drawWinCells(cells) {
@@ -72,8 +171,9 @@ function drawWinCells(cells) {
 
 
 /* Test Function */
+
 /* Победа первого игрока */
-function testWin () {
+function testWin() {
     clickOnCell(0, 2);
     clickOnCell(0, 0);
     clickOnCell(2, 0);
@@ -84,7 +184,7 @@ function testWin () {
 }
 
 /* Ничья */
-function testDraw () {
+function testDraw() {
     clickOnCell(2, 0);
     clickOnCell(1, 0);
     clickOnCell(1, 1);
@@ -97,6 +197,6 @@ function testDraw () {
     clickOnCell(2, 2);
 }
 
-function clickOnCell (row, col) {
+function clickOnCell(row, col) {
     findCell(row, col).click();
 }
